939d6ca1766924ebc2cb554a0c271cf530ca5ca9
DemuraEvgeniy <34704791+DemuraEvgeniy@users.noreply.github.com>
2019-01-08 14:43:59 +0300

    update mail.php

--- LAMBDA/mail.php
+++ LAMBDA/mail.php
@@ -5,21 +5,23 @@ $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
 $name = $_POST['name'];
-$phone = $_POST['number'];
+$number = $_POST['number'];
 $email = $_POST['email'];
-$date = $_post['date']
+$date = $_POST['date'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.yandex.ru';  				// Specify main and backup SMTP servers
+//$mail->Host = 'aspmx.l.google.com';  				// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'evg.demura@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'evg21051984'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'TLS';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587; // TCP port to connect to / этот порт может отличаться у других провайдеров
+//$mail->Port = 25; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('evg.demura@yandex.ru'); // от кого будет уходить письмо?
-$mail->addAddress('evg.demyra@gmail.com');     // Кому будет уходить письмо
+$mail->addAddress('evg.demura@yandex.ru');     // Кому будет уходить письмо
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -28,13 +30,14 @@ $mail->addAddress('evg.demyra@gmail.com');     // Кому будет уходи
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, <br> Когда: ' .$date. ';' '<br> Почта этого пользователя: ' .$email. '<br> Номер заказа' .$number.;
+$mail->Subject = 'Заявка с сайта Lambda';
+$mail->Body    = '' .$name . ' оставил заявку; <br> Когда: ' .$date. '; <br> Почта этого пользователя: ' .$email. ';<br> Номер заказа :' .$number;
 $mail->AltBody = '';
 
 if(!$mail->send()) {
-    echo 'Error';}
-// } else {
-//  header('location: index.html');
-// }
+    echo 'Error';
+}
+ else {
+  header('location: index.html');
+ }
 ?>
