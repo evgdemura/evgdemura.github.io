2c67b4e2391a4c6623f3216a677a78ddcf831bf2
DemuraEvgeniy <34704791+DemuraEvgeniy@users.noreply.github.com>
2019-01-15 17:25:08 +0300

    add lambda

---
+++ lambda/js.js
@@ -0,0 +1,67 @@
+$('#form-date').datepicker({
+	dateFormat: "dd-mm-yy"
+});
+
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 400) {
+		$('.scroll-button').css('transform', 'translate(0px)');
+	} else {
+		$('.scroll-button').css('transform', 'translate(150px)');
+	}
+
+});
+
+$('.scroll-button').click(function () {
+	$('html,body').animate({
+		scrollTop: 0
+	}, 400);
+
+});
+
+function scrollToElem(elem, time) {
+	var target = $(elem).offset().top;
+	$('html,body').animate({
+		scrollTop: target
+	}, time || 400)
+}
+$('[href="#about"]').click(function () {
+	scrollToElem("#about")
+});
+$('[href="#ingredients"]').click(function () {
+	scrollToElem("#ingredients")
+});
+$('[href="#menu"]').click(function () {
+	scrollToElem("#menu")
+});
+$('[href="#footer"]').click(function () {
+	scrollToElem("#footer", 800)
+});
+$('[href="#reservation"]').click(function () {
+	scrollToElem("#reservation", 800)
+});
+//=============mail====================
+$("#form").submit(function () {
+	$.ajax({
+		type: "POST",
+		url: "mail.php",
+		data: $("#form").serialize()
+	}).done(function () {
+		alert('Спасибо!')
+	}).fail(function () {
+		alert('Ошибка!')
+	})
+	// })
+})
+
+// ==================отключение wow.js на маленьком разрешении===============
+	var width = $(window).width()
+$(window).resize(function () {
+if ($(window).width() >= 650) {
+		new WOW().init();
+} else {
+
+	if ($(window).width() != width) {
+		window.location.reload();
+		width = $(window).width();}
+}
+})
---
+++ lambda/mail.php
@@ -0,0 +1,42 @@
+<?php
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = trim(htmlspecialchars($_POST['name']));//Получаем имя
+$email = trim(htmlspecialchars($_POST['email']));//Получаем адрес эл. почты
+$number = trim(htmlspecialchars($_POST['number']));
+$date = trim(htmlspecialchars($_POST['date']));
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.yandex.ru';  				// Specify main and backup SMTP servers
+//$mail->Host = 'aspmx.l.google.com';  				// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'evg.demura@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'evg21051984'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'TLS';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587; // TCP port to connect to / этот порт может отличаться у других провайдеров
+//$mail->Port = 25; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('evg.demura@yandex.ru'); // от кого будет уходить письмо?
+$mail->addAddress('evg.demura@yandex.ru');     // Кому будет уходить письмо
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с сайта Lambda';
+$mail->Body    = '' .$name . ' оставил заявку; <br> Когда: ' .$date. '; <br> Почта этого пользователя: ' .$email. ';<br> Номер заказа :' .$number;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+}
+ else {
+  header('location: index.html');
+ }
+?>
